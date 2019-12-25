f0ef9e757311291eeb344005d1acfb7f6883805a
Vincent Dilly <vdilly.prod@gmail.com>
2019-12-25 13:01:54 +0100

    set query that cause the issue

---
+++ src/pages/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <Layout>
+    <h1>Welcome to my blog :)</h1>
+  </Layout>
+</template>
+
+<page-query>
+query($page: Int){
+  tags: allWordPressPostTag {
+    edges {
+      node {
+        slug
+        belongsTo(perPage: 12, page: $page) @paginate {
+          pageInfo {
+            totalPages
+            currentPage
+          }
+          edges {
+            node {
+              ...on WordPressPost{
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import { Pager } from "gridsome";
+import Post from "~/components/Post.vue";
+
+export default {
+  components: {
+    Pager,
+    Post
+  },
+  metaInfo: {
+    title: "Welcome to my blog :)"
+  }
+};
+</script>
